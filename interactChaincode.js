5e531a7465f83b9a759a97f197c8be93c9dabd7e
Maria Teresa Nieto <mariateresa.nietogalan@telefonica.com>
2021-08-24 18:18:02 +0200

    create functions

--- interactChaincode.js
+++ interactChaincode.js
@@ -6,38 +6,66 @@ const path = require('path');
 const ccpPath = path.resolve(__dirname, 'connection.json');
 const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
 const ccp = JSON.parse(ccpJSON);
+let network, gateway; 
 
+async function connectNetwork(){
+    const walletPath = '/tmp/hfnode/wallet';
+    const wallet = await Wallets.newFileSystemWallet(walletPath);
+    gateway = new Gateway();
+
+    await wallet.get("adminCA");
+    await gateway.connect(ccp, {
+        wallet: wallet,
+        identity: "adminCA",
+        discovery: {
+            enabled: true,
+            asLocalhost: true,
+        }
+    });
+   network = await gateway.getNetwork('channel1');
+}
+async function disconnectNetwork(){
+    await gateway.disconnect();
+
+}
+async function createCar(id, make, model, color, owner){
+    try{
+        const contract = network.getContract('fabcar');
+
+        let result = await contract.submitTransaction('createCar', id, make, model, color, owner);
+    
+        console.log('Transaction has been submitted with result', result.toString());
+    } catch(err){
+        throw err;
+    }
+   
+}
+
+async function queryCar(id){
+    try{
+        const contract = network.getContract('fabcar');
+        result = await contract.evaluateTransaction('queryCar', id);
+        console.log('Get from chaincode with result', result.toString());
+    } catch(err){
+        throw err;    
+    }
+}
 
 async function main() {
     try {
-
-        const walletPath = '/tmp/hfnode/wallet';
-        const wallet = await Wallets.newFileSystemWallet(walletPath);
-        const gateway = new Gateway();
-
-        await wallet.get("adminCA");
-        await gateway.connect(ccp, {
-            wallet: wallet,
-            identity: "adminCA",
-            discovery: {
-                enabled: true,
-                asLocalhost: true,
-            }
-        });
-        const network = await gateway.getNetwork('channel1');
-        console.log("****", network)
-            // Get the contract from the netw ork.getContract('cc');
-        const contract = network.getContract('fabcar');
-
-        let result = await contract.submitTransaction('createCar', 'CAR36', 'Mazda', "MAzda3", "white", "Maritere");
-
-        console.log('Transaction has been submitted with result', result.toString());
-
-        result = await contract.evaluateTransaction('queryCar', "CAR36");
-        console.log('Get from chaincode with result', result.toString());
+    console.log("Connecting network");
+    await connectNetwork();
+    console.log("Creating car");
+    await createCar('CAR37', 'Mazda', "MAzda3", "white", "Maritere")
+    console.log("Querying car");
+    await queryCar('CAR37');
+    console.log("Everything went ok...disconnecting")
+    await disconnectNetwork()
 
 
-        await gateway.disconnect();
+
+
+
     } catch (err) {
 
         console.error(`Failed to submit transaction: ${err}`);
